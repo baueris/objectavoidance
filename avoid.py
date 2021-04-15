b9ffed766b4dc065fdf052718ce66dfc7e8db3a1
Nathaniel Rankin <rankinna@bc.edu>
2021-04-15 19:23:25 -0400

    working boxes, lines

--- avoid.py
+++ avoid.py
@@ -1,119 +1,117 @@
 # Zig zag
-direction = "r"
-avoidDirection = "d"
+#
+import time
+
+xDirection = "r"
+yDirection = "d"
 x = 0
 y = 0
+returnToY = 0
+originalX = 0
 
 avoiding = False
 
 room = [[0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,2,2,2,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,2,0,0,0,0],
+        [0,0,0,0,2,2,2,0,0,0],
+        [0,0,0,0,0,2,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0]]
 
-# room = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-#         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0]]
+
+
+# room = [[0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0],
+#         [0,0,0,0,0,0,0,0,0,0]]
+
+
+def is_finished():
+    for r in range(len(room)):
+        for c in range(len(room[r])):
+            if (room[r][c] == 0):
+                return False
+    return True
 
 print ("Started at coordinate " + str(x) + ", " + str(y))
 room[y][x] = 1
 
-while (True):
-    if(not avoiding):
-        if (direction == "r"):
-            if (x != len(room[1]) - 1):
-                if (room[y][x + 1] != 2):
-                    x = x + 1
-                    room[y][x] = 1;
-                    print("Moved to coordinate " + str(x) + ", " + str(y))
-                    continue
-                else:
-                    avoiding = True
-                    avoidDirection = "d"
-                    returnToY = y
-                    print("Found an obstacle")
-                    continue
-            else:
-                if (y != len(room) - 1):
-                    y = y + 1
-                    direction = "l"
-                    room[y][x] = 1;
-                    print("Moved to coordinate " + str(x) + ", " + str(y))
-                    continue
-                else:
-                    print("Scan completed")
-                    break
+while (not is_finished()):
+    if (xDirection == "r"):
+        if (x != len(room[y]) - 1 and room[y][x + 1] != 2):
+            x = x + 1
+            room[y][x] = 1;
+            print("+Moved to coordinate " + str(x) + ", " + str(y))
         else:
-            if (x != 0):
-                if (room[y][x - 1] != 2):
-                    x = x - 1
-                    room[y][x] = 1;
-                    print("Moved to coordinate " + str(x) + ", " + str(y))
-                    continue
-                else:
-                    avoiding = True
-                    returnToY = y
-                    avoidDirection = "d"
-                    print("Found an obstacle")
-                    continue
-            else:
+            if (yDirection == "d"):
                 if (y != len(room) - 1):
                     y = y + 1
-                    direction = "r"
-                    room[y][x] = 1;
-                    print("Moved to coordinate " + str(x) + ", " + str(y))
-                    continue
+                    room[y][x] = 1
+                    xDirection = "l"
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
                 else:
-                    print("Scan completed")
-                    break
+                    yDirection = "u"
+                    xDirection = "l"
+                    y = y - 1
+                    room[y][x] = 1
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
+            else:
+                if (y != 0):
+                    y = y - 1
+                    room[y][x] = 1
+                    xDirection = "l"
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
+                else:
+                    yDirection = "d"
+                    xDirection = "l"
+                    y = y + 1
+                    room[y][x] = 1
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
+                    y = y + 1
+                    room[y][x] = 1
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
     else:
-        # Avoiding
-        if (avoidDirection == "d"):
-            if (y != len(room) - 1 and room[y - 1][x] != "2"):
-                y = y + 1
-                print("*Moved to coordinate " + str(x) + ", " + str(y)) # "*" means avoiding
-            elif (room[y - 1][x] != "2"):
-                avoidDirection = "u"
-                y = y + 1
-                print("*Moved to coordinate " + str(x) + ", " + str(y))
-                continue
-            elif (direction == "l"):
-                avoidDirection = "r"
-                continue
+        if (x != 0  and room[y][x - 1] != 2):
+            x = x - 1
+            room[y][x] = 1;
+            print("+Moved to coordinate " + str(x) + ", " + str(y))
+        else:
+            if (yDirection == "d"):
+                if (y != len(room) - 1):
+                    y = y + 1
+                    room[y][x] = 1
+                    xDirection = "r"
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
+                else:
+                    yDirection = "u"
+                    xDirection = "r"
+                    y = y - 1
+                    room[y][x] = 1
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
             else:
-                avoidDirection = "l"
-                continue
-            # Test if can navigate obstacle after moving down 1
-            if (direction == "r"):
-                if (x != len(room[1]) - 1 and room[y][x + 1] != "2"):
-                    x = x + 1;
-                    print("*Moved to coordinate " + str(x) + ", " + str(y))
-            else:
-                if (x != 0 and room[y][x - 1] != "2"):
-                    x = x - 1;
-                    print("*Moved to coordinate " + str(x) + ", " + str(y))
-
-print(room)
+                if (y != 0):
+                    y = y - 1
+                    room[y][x] = 1
+                    xDirection = "r"
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
+                else:
+                    yDirection = "d"
+                    xDirection = "r"
+                    y = y + 1
+                    room[y][x] = 1
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
+                    y = y + 1
+                    room[y][x] = 1
+                    print("+Moved to coordinate " + str(x) + ", " + str(y))
+    print(room)
+    time.sleep(0.1)
